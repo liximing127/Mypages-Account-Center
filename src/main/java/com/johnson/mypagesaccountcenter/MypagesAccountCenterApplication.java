418add099924b0778b5c14e454c1d737c2ec4981
liximing <liximing127@gmail.com>
2020-03-17 00:03:19 +0900

    init

---
+++ src/main/java/com/johnson/mypagesaccountcenter/MypagesAccountCenterApplication.java
@@ -0,0 +1,13 @@
+package com.johnson.mypagesaccountcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypagesAccountCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypagesAccountCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johnson/mypagesaccountcenter/MypagesAccountCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnson.mypagesaccountcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypagesAccountCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
